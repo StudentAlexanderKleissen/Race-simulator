f86deb3c02f8b8a95db88c6d5ccdf01b8427747b
AlexanderKleissen <74188354+AlexanderKleissen@users.noreply.github.com>
2021-09-13 15:11:15 +0200

    Interfaces en classes toegevoegd aan main

---
+++ Model/Competition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Competition
+    {
+        public List<IParticipant> Participants;
+        public Queue<Track> Tracks;
+
+
+    }
+}
