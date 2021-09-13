6ddf5e2b5a6ce4ecb4951740c87f10d7ff8de372
AlexanderKleissen <74188354+AlexanderKleissen@users.noreply.github.com>
2021-09-13 14:19:58 +0200

    Add project files.

---
+++ Race simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Race_simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
