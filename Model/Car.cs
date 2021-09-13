c08de5cd27bd3599fffb32e8794a1f9179320ce2
AlexanderKleissen <74188354+AlexanderKleissen@users.noreply.github.com>
2021-09-13 14:53:01 +0200

    Een aantal classes toegevoegd

---
+++ Model/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Model
+{
+    public class Car : IEquipment
+    {
+        public Car(int quality, int performance, int speed, bool isBroken)
+        {
+            Quality = quality;
+            Performance = performance;
+            Speed = speed;
+            IsBroken = isBroken;
+        }
+
+        public int Quality { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Performance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsBroken { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Model/IParticipant.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Model
+{
+    public interface IParticipant
+    {
+        public enum TeamColors
+        {
+            Red,
+            Green,
+            Yellow,
+            Grey,
+            Blue
+        }
+
+        string Name { get; set; }
+        public int Points { get; set; }
+        public IEquipment Equipment { get; set; }
+        public TeamColors TeamColor { get; set; }
+    }
+}
